097d6a3c23b5a2bc5eb619329b0dc5f2eae76311
Hossein Amirhosseini <Hamirhosseini3@gmail.com>
2023-08-20 16:35:21 +0330

    fix: Fix assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        DEFAULT: '#00DC82',
+        50: '#E6FDF4',
+        100: '#D0FCE9',
+        200: '#A1F6D0',
+        300: '#72F1B7',
+        400: '#43EC9E',
+        500: '#00DC82',
+        600: '#00A86A',
+        700: '#007651',
+        800: '#004D38',
+        900: '#00241F',
+      },
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    }
   },
   plugins: [],
 }
